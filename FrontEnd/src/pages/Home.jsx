cfa2913c53be1f81df04d2e071c65c889110800d
Quentin <quentin.caflo@gmail.com>
2024-12-31 08:53:49 +0100

    feat(app): add banner and responsive styling

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,5 +1,27 @@
+import logo from "../../public/images/bank-tree.avif";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <main>
+      <div className="hero">
+        <img src={logo} alt="Bank Tree" />
+        <section className="hero__content">
+          <h2 className="sr-only">Promoted Content</h2>
+          <p className="hero__content-subtitle">
+            No fees. <br />
+            No minimum deposit. <br />
+            High interest rates.
+          </p>
+          <p className="hero__content-text">
+            Open a savings account with Argent Bank today!
+          </p>
+        </section>
+      </div>
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
